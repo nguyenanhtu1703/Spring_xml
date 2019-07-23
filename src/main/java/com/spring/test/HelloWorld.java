298c6a1dc9a0ed30092a9a807f1958161962e6d5
nguyenanhtu2 <nguyenanhtuinc@gmail.com>
2019-07-23 20:07:09 +0200

    fc

---
+++ src/main/java/com/spring/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.spring.test;
+
+public class HelloWorld {
+	public String message;
+	public void setMessage(String s) {
+		this.message = s;
+	}
+	public void getMessage() {
+		System.out.println(message);
+	}
+}
